8ab9047fed236a970580e740918706588df47b8f
DavidZhong10 <1667015173@qq.com>
2020-09-27 22:24:04 +0800

    测试springboot-demo代码上传到github上

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("--------启动springboot-demo项目了---------");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public void hello(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+
+        int id = Integer.parseInt(req.getParameter("id"));
+        if(id > 0){
+            resp.sendRedirect("/ok.html");
+        }else{
+            req.getRequestDispatcher("/fail.html").forward(req,resp);
+        }
+        System.out.println("id值为：" + req.getParameter("id"));
+    }
+
+}
